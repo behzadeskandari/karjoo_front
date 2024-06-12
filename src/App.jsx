d31c13695dda153055b951a352bdf90d351fe973
105624033 <105624033@RESPLN-PC71>
2024-06-12 11:08:20 +0330

    colorizing the text and also breaking code into compoennts

--- src/App.jsx
+++ src/App.jsx
@@ -11,51 +11,55 @@ import Select from "react-select";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Form } from "react-bootstrap";
 import HeaderAndNavigationMenu from "./components/Menu/HeaderAndNavigationMenu";
-import Carousel from './components/Carousel/Carousel';
-import {JobCategoryOptions} from './Constant/MainPage/DropDown/JobCategory';
-
-
+import Carousel from "./components/Carousel/Carousel";
+import { JobCategoryOptions } from "./Constant/MainPage/DropDown/JobCategory";
+import {
+  HeaderBanner,
+  HeaderBannerCity,
+  JobSeekingType,
+} from "./Constant/MainPage/Text/index";
+import AnimatedCounter from "./Constant/MainPage/Counter/AnimatedCounter";
 function App() {
-  const HeaderBanner = "اگهی شغل در";
-  const HeaderBannerCity = "شهر";
-  const JobSeekingType = "دنبال چه شغلی میگردید";
+  //refs
+  const textRefJobSeeking = React.useRef();
+  const textRefHeaderBannerCity = React.useRef();
+  const textRefHeaderBanner = React.useRef();
+  const numberRefCounterResume = React.useRef();
+  const numberRefCounterCity = React.useRef();
+  //refs
+  ////////////////////////states
   const [matches, setMatches] = useState(
     window.matchMedia("(min-width: 768px)").matches
   );
-
   const [isDesktop, setIsDesktop] = useState(true);
   const [isClearable, setIsClearable] = useState(true);
   const [isSearchable, setIsSearchable] = useState(true);
   const [isDisabled, setIsDisabled] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isRtl, setIsRtl] = useState(true);
+  const [isSetColor, setIsColor] = useState();
+  const [isShowProvince, setIsShowProvince] = useState(false);
   const [dimensions, setDimensions] = React.useState({
     height: window.innerHeight,
     width: window.innerWidth,
   });
-
-  
-  // const JobCategoryOptions = [
-  //   { value: "chocolate", label: "Chocolate" },
-  //   { value: "strawberry", label: "Strawberry" },
-  //   { value: "vanilla", label: "Vanilla" },
-  // ];
+  ////////////////////////states
+  //options
   const CityOptions = [
     { value: "1", label: "تهران" },
     { value: "2", label: "البرز" },
     { value: "3", label: "آذربایجان غربی" },
     { value: "4", label: "آذربایجان شرقی" },
   ];
+  //options
+  //Effects
   useEffect(() => {
-    // console.log(matches, "matches");
     const debouncedHandleResize = debounce(function handleResize() {
       setDimensions({
         height: window.innerHeight,
         width: window.innerWidth,
       });
     }, 50);
-    // console.log(dimensions, "dimensions");
-    // console.log(isDesktop, "isDesktop");
 
     if (dimensions.width < 900) {
       setIsDesktop(false);
@@ -68,6 +72,73 @@ function App() {
       window.removeEventListener("resize", debouncedHandleResize);
     };
   }, [isDesktop, dimensions]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      SetColor(
+        textRefJobSeeking,
+        textRefHeaderBannerCity,
+        textRefHeaderBanner,
+        numberRefCounterResume,
+        numberRefCounterCity
+      );
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  //Effects
+  //Arrays
+
+  const colors = ["#e74c3c", "#8e44ad", "#3498db", "#e67e22", "#2ecc71"];
+  const colorsText = [
+    "#1679AB",
+    "#1679AB",
+    "#BC5A94",
+    "#AF47D2",
+    "#26355D",
+    "#686D76",
+    "#F1E5D1",
+  ];
+
+  //Arrays
+
+  //region Func
+  function SetColor(
+    textRefJobSeeking,
+    textRefHeaderBannerCity,
+    textRefHeaderBanner,
+    numberRefCounterResume,
+    numberRefCounterCity
+  ) {
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    if (textRefJobSeeking.current) {
+      textRefJobSeeking.current.style.color = color;
+      textRefJobSeeking.current.style.transition = "3s";
+    }
+    if (textRefHeaderBannerCity.current) {
+      textRefHeaderBannerCity.current.style.color = color;
+      textRefHeaderBannerCity.current.style.transition = "3s";
+    }
+    if (textRefHeaderBanner.current) {
+      textRefHeaderBanner.current.style.color = color;
+      textRefHeaderBanner.current.style.transition = "3s";
+    }
+    const colorstext =
+      colorsText[Math.floor(Math.random() * colorsText.length)];
+    if (numberRefCounterResume.current) {
+      numberRefCounterResume.current.style.color = colorstext;
+      numberRefCounterResume.current.style.transition = "3s";
+    }
+    //console.log(numberRefCounterCity, "numberRefCounterCity");
+    if (numberRefCounterCity.current) {
+      numberRefCounterCity.current.style.color = colorstext;
+      numberRefCounterCity.current.style.transition = "3s";
+    }
+  }
+
   function debounce(fn, ms) {
     let timer;
     return (_) => {
@@ -79,6 +150,15 @@ function App() {
     };
   }
 
+  function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  const handleProvinceChange = (e) => {
+    console.log(e, "handleProvince");
+  };
+  //end region Func
+
   return (
     <>
       <CssBaseline />
@@ -86,18 +166,25 @@ function App() {
       <div style={{ padding: "16px", textAlign: "right" }}>
         <h1>
           <div className="HeaderBanner">
-            <span>{HeaderBanner}</span>
-            <span style={{ color: "blue" }}>50,507</span>
+            <span ref={textRefHeaderBanner}>{HeaderBanner}</span>
+            <span className="color-blue">
+              <AnimatedCounter
+                targetNumber={50507}
+                ref={numberRefCounterResume}
+              />
+            </span>
           </div>
           <div className="HeaderBanner">
-            <span>{HeaderBannerCity}</span>
-            <span style={{ color: "blue", padding: "0px 7px 0px 7px" }}>
-              505
+            <span ref={textRefHeaderBannerCity}>{HeaderBannerCity}</span>
+            <span className="color-blue padd-text">
+              <AnimatedCounter targetNumber={505} ref={numberRefCounterCity} />
             </span>
           </div>
         </h1>
         {/* ImageTAG Goes Here */}
-        <h3 className="margin-top-60">{JobSeekingType}</h3>
+        <h3 className="margin-top-60 textholder" ref={textRefJobSeeking}>
+          {JobSeekingType}
+        </h3>
         <section className="row">
           <div
             className={`${
@@ -162,17 +249,42 @@ function App() {
                 isRtl={isRtl}
                 isSearchable={isSearchable}
                 name="City"
+                onChange={(e) => handleProvinceChange(e)}
                 options={CityOptions}
                 placeholder={"شهر"}
               />
             </div>
+            {isShowProvince ? (
+              <div
+                className={`${
+                  isDesktop
+                    ? "col-4 col-lg-4 col-md-4 col-sm-4 p-4 "
+                    : "col-12 col-lg-12 col-md-12 col-sm-12 margin-2"
+                }`}
+              >
+                <Select
+                  className={`${
+                    isDesktop ? "p-4" : ""
+                  }basic-single margin-top-60 `}
+                  classNamePrefix="select"
+                  defaultValue={CityOptions[0]}
+                  isDisabled={isDisabled}
+                  isLoading={isLoading}
+                  isClearable={isClearable}
+                  isRtl={isRtl}
+                  isSearchable={isSearchable}
+                  name="City"
+                  options={CityOptions}
+                  placeholder={"استان"}
+                />
+              </div>
+            ) : null}
           </div>
         </section>
-        <Carousel/>
+        <Carousel />
       </div>
     </>
   );
 }
-
 
 export default App;
--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,57 +2,57 @@ import React from "react";
 import ReactCardSlider from "react-card-slider-component";
 
 export default function Carousel() {
+  const sliderClick = (e) => {
+    console.log(e, "clickd slider");
+  };
   const slides = [
     {
       image: "https://picsum.photos/200/300",
       title: "This is a title",
       description: "This is a description",
-    //   clickEvent: sliderClick,
+      clickEvent: sliderClick,
     },
     {
       image: "https://picsum.photos/600/500",
       title: "This is a second title",
       description: "This is a second description",
-    //   clickEvent: sliderClick,
+      clickEvent: sliderClick,
     },
     {
       image: "https://picsum.photos/700/600",
       title: "This is a third title",
       description: "This is a third description",
-    //   clickEvent: sliderClick,
+      clickEvent: sliderClick,
     },
     {
       image: "https://picsum.photos/500/400",
       title: "This is a fourth title",
       description: "This is a fourth description",
-    //   clickEvent: sliderClick,
+      clickEvent: sliderClick,
     },
     {
       image: "https://picsum.photos/200/300",
       title: "This is a fifth title",
       description: "This is a fifth description",
-    //   clickEvent: sliderClick,
+      clickEvent: (e) => sliderClick(e),
     },
     {
       image: "https://picsum.photos/800/700",
       title: "This is a sixth title",
       description: "This is a sixth description",
-     // clickEvent: sliderClick,
+      clickEvent: (e) => sliderClick(e),
     },
     {
       image: "https://picsum.photos/300/400",
       title: "This is a seventh title",
       description: "This is a seventh description",
-     // clickEvent: sliderClick,
+      clickEvent: (e) => sliderClick(e),
     },
   ];
 
-  const sliderClick = (e)=> {
-    console.log(e,'clickd slider')
-  }
   return (
-  <div>
-    <ReactCardSlider slides={slides}/>
-  </div>
+    <div className="d-flex justify-content-center">
+      <ReactCardSlider slides={slides} />
+    </div>
   );
 }
